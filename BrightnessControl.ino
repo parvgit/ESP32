e0155a1191450603bc0410bf29747e1a1ace0847
Parveen Kumar <34051944+parvgit@users.noreply.github.com>
2023-12-27 14:55:49 +0530

    Update BrightnessControl.ino

--- BrightnessControl.ino
+++ BrightnessControl.ino
@@ -1,14 +1,7 @@
 #include <dummy.h>
-/*
- * https://circuits4you.com
- * ESP32 LED Blink Example
- * Board ESP23 DEVKIT V1
- * 
- * ON Board LED GPIO 2
- */
 
 
-#define RED 4
+#define RED 4  //Connect LED on this 
 #define GREEN 16
 #define BLUE 17
 
@@ -20,18 +13,7 @@ void setup() {
 }
 
 void loop() {
-  //  analogWrite(RED,0);
-  //  delay(1000);
-  //  analogWrite(RED,50);
-  //  delay(1000); 
-  //  analogWrite(RED,100);
-  //  delay(1000);
-  //  analogWrite(RED,150);
-  //  delay(1000); 
-  //  analogWrite(RED,200);
-  //  delay(1000); 
-  //  analogWrite(RED,255);
-  //  delay(1000);
+
   for(int i=0;i<255;i++)
   {
     analogWrite(RED,i);
